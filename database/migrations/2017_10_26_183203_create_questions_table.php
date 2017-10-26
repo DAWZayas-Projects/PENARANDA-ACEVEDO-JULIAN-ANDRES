77fcdb5a2439436d33a47b88fa87a7a63bb38424
Rowleen <julianpeacevedo@gmail.com>
2017-10-26 20:53:03 +0200

    Creadas las migraciones de preguntas, respuestas y votos de preguntas.

---
+++ database/migrations/2017_10_26_183203_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('category');
+            $table->string('state')->default('pending');
+            $table->string('text', 140);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
