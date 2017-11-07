2c77fd29b393b787313ca978d2d710b4f9e3e97a
Rowleen <julianpeacevedo@gmail.com>
2017-11-07 18:03:44 +0100

    Añadida nueva ruta para apuntar al index

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 use App\Question;
+Route::get('/', function() {
+   return view('layout');
+});
 Route::get('/questions', 'QuestionsController@index');
 Route::get('/questions/{question}', 'QuestionsController@showQuestion');
