5c9e7d4617b42ef4d54f4cb9c7238d4ab09fd609
Rowleen <julianpeacevedo@gmail.com>
2017-11-07 17:48:23 +0100

    Se han modificado y añadido nuevas migraciones a la aplicación.

--- database/migrations/2016_11_06_200246_create_categories_table.php
+++ database/migrations/2016_11_06_200246_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
-           $table->engine = 'InnoDB';
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id')->unique();
-            $table->string('state')->default('pending');
-            $table->string('text', 140);
+            $table->text('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('categories');
     }
 }
